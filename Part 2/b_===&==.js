a3fa7229421fc360e69f01e02d1385b63c5797d3
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-09-06 12:02:31 +0530

    javaScript

---
+++ Part 2/b_===&==.js
@@ -0,0 +1,16 @@
+let a = 123;
+let b = '123';
+
+console.log(a==b);  /* true as content is same */
+
+console.log(a===b); /* false as datatype is not same */
+
+console.log("________________________________________");
+
+console.log(0 == " ");
+console.log(0 === " ");
+
+console.log("________________________________________");
+
+console.log(null == undefined);
+console.log(null === undefined);
---
+++ Part 2/c_traffic_light.js
@@ -0,0 +1,9 @@
+let light = "red";
+
+if(light === "red" || "Red" || "RED"){
+    console.log("Stop");
+} else if(light === "green" || "Green" || "GREEN"){
+    console.log("go");
+}else{
+    console.log("Wait");
+}
