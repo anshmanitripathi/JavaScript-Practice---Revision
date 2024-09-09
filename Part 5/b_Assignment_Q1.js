13d41336f0f389384fecfa14acadb20250ed201f
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-09-09 13:13:17 +0530

    javaScript

---
+++ Part 5/b_Assignment_Q1.js
@@ -0,0 +1,5 @@
+// Createaprogramthatgeneratesarandomnumberrepresentingadiceroll.[Thenumbershouldbebetween1and6].
+
+let dice = Math.floor(Math.random() * 6) +1;
+
+console.log(`Roll dice , it gave ${dice}`)
