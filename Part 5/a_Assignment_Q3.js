dd2c8c11a70dc99073fca74779a99bdd4292774f
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-09-09 13:30:37 +0530

    javaScript

---
+++ Part 5/a_Assignment_Q3.js
@@ -0,0 +1,17 @@
+// CreateanobjectPersonwiththeirname,ageandcity.
+// Edittheircity’soriginalvaluetochangeitto“NewYork”.
+// AddanewpropertycountryandsetittotheUnitedStates
+
+let Person = {
+    name : "Ansh",
+    age : 20,
+    city : "Gorakhpur",
+}
+
+console.log(Person);
+
+Person.city = "Greater Noida";
+console.log(Person);
+
+Person.Country = "India";
+console.log(Person);
